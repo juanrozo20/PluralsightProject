c58cd8f25b425180995061f75d7bd31095b0180c
juan.rozo <juan.rozo@globant.com>
2024-12-19 15:53:22 -0500

    Initial Commit

---
+++ PluralsightProject/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  PluralsightProject
+//
+//  Created by Juan Esteban Rozo Mendez on 19/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ PluralsightProject/PluralsightProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PluralsightProjectApp.swift
+//  PluralsightProject
+//
+//  Created by Juan Esteban Rozo Mendez on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PluralsightProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
